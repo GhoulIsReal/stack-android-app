9a80a040ef71bcc9b93d87181813c0653f3aed0e
Daily120 <forzhacker@mail.ru>
2019-06-04 10:36:26 +0200

    getMacAddress function

--- app/src/main/java/com/stackpointflow/stackapp/MainActivity.java
+++ app/src/main/java/com/stackpointflow/stackapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.stackpointflow.stackapp;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String getMacAddress() {
+        WifiManager manager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = manager.getConnectionInfo();
+        String address = info.getMacAddress();
+
+        return address;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t1 = (TextView) findViewById(R.id.mac_text);
+        t1.setText(getMacAddress());
     }
 }
